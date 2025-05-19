7d77f6f432650985cc212ee7bb4f31992d813102
yashhadade <yashhadade13@gmail.com>
2025-05-19 23:52:18 +0530

    Hire me button according to scroll

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./styles.scss"
-import {useNavigate} from 'react-router-dom'
-import {Animate} from 'react-simple-animate'
+import { useNavigate } from 'react-router-dom'
+import { Animate } from 'react-simple-animate'
 import About from '../about'
 import Skill from '../skills'
 import Resume from '../resume'
@@ -9,47 +9,49 @@ import resume from '../../assets/Yash_Hadade_resume.pdf'
 import Portfolio from '../portfolio'
 import Contact from '../contact'
 const Home = () => {
-  const navigate = useNavigate()
-  const handleNavigateToContactMe=()=>{
-    navigate("/contact")
-  }
+  const handleScrollToContact = () => {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <>
-    
-   <section id='home' className='home'>
-    <div className='home_text-wrapper'>
-        <h1>
-          Hello I'm Yash
-          <br/>
-          Fullstack developer
-        </h1>
-      </div>
-      <Animate
-      play
-      duration={1.5}
-      delay={1}
-      start={{
-        transform:'translateY(550px)'
-      }}
-      end={{
-        transform:'translateY(0px)'
-      }}
-      >
-      <div className='home_contact-me'>
-         <button onClick={handleNavigateToContactMe}>Hire Me</button>
-         <a href={resume} download='resume'>
-          <button>
-         Resume
-          </button>
-           </a>
-      </div>
-      </Animate>
-    </section>
-     <About/>
-     <Skill/>
-     <Resume/>
-     <Portfolio/>
-     <Contact/>
+
+      <section id='home' className='home'>
+        <div className='home_text-wrapper'>
+          <h1>
+            Hello I'm Yash
+            <br />
+            Fullstack developer
+          </h1>
+        </div>
+        <Animate
+          play
+          duration={1.5}
+          delay={1}
+          start={{
+            transform: 'translateY(550px)'
+          }}
+          end={{
+            transform: 'translateY(0px)'
+          }}
+        >
+          <div className='home_contact-me'>
+            <button onClick={handleScrollToContact}>Hire Me</button>
+            <a href={resume} download='resume'>
+              <button>
+                Resume
+              </button>
+            </a>
+          </div>
+        </Animate>
+      </section>
+      <About />
+      <Skill />
+      <Resume />
+      <Portfolio />
+      <Contact />
     </>
   )
 }
